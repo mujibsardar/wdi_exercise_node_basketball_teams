fbef0e1c0409cc342ca318d728162728aa0a6439
Daniel Macauyag <dmacauyag92@gmail.com>
2017-05-04 15:23:01 -0700

    created controller for teams

---
+++ controllers/teams.js
@@ -0,0 +1,33 @@
+const Team = require('../models/Teams.js')
+
+module.exports = {
+  index: (req, res) => {
+    Team.find({}, (err, teamsFromDB) => {
+      res.json(teamsFromDB)
+    })
+  },
+
+  create: (req, res) => {
+    Team.create(req.body, (err, newTeam) => {
+      res.json({success: true, team: newTeam})
+    })
+  },
+
+  show: (req, res) => {
+    Team.findById(req.params.id, (err, singleTeam) => {
+      res.json(singleTeam)
+    })
+  },
+
+  update: (req, res) => {
+    Team.findByIdAndUpdate(req.params.id, req.params.body, {new: true}, (err, updatedTeam) => {
+      res.json({success: true, team: updatedTeam})
+    })
+  },
+
+  delete: (req, res) => {
+    Team.findByIdAndRemove(req.params.id, (err, deletedTeam) => {
+      res.json({success: true, removed: deletedTeam})
+    })
+  }
+}
